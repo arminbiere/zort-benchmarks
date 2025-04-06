f699de403962111de0eb91955b7ce907a93f426e
Armin Biere <biere@cs.uni-freiburg.de>
2025-04-06 19:18:24 +0200

    parsing almost done

--- zort.c
+++ zort.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <ctype.h>
 #include <fcntl.h>
 #include <stdarg.h>
 #include <stdbool.h>
@@ -20,8 +21,20 @@ struct zummary {
   } limit;
 };
 
+struct benchmark {
+  size_t number;
+  char *path;
+  char *name;
+};
+
 static char *line;
 static size_t size_line, capacity_line;
+
+static struct zummary *zummaries;
+static size_t size_zummaries, capacity_zummaries;
+
+static struct benchmark *benchmarks;
+static size_t size_benchmarks, capacity_benchmarks;
 
 static const char *benchmarks_path;
 static const char *directory_path;
@@ -45,8 +58,8 @@ static void push_char(int ch) {
   assert(ch != EOF);
   assert(ch != '\n');
   if (size_line == capacity_line) {
-    size_line = size_line ? 2 * size_line : 1;
-    line = realloc(line, size_line);
+    capacity_line = capacity_line ? 2 * capacity_line : 1;
+    line = realloc(line, capacity_line);
     if (!line)
       out_of_memory("reallocating line");
   }
@@ -82,6 +95,54 @@ static bool read_line(FILE *file, const char *path) {
   return true;
 }
 
+static void parse_benchmark(struct benchmark *benchmark, const char *path) {
+  char *p = line;
+  size_t number = 0;
+  if (!isdigit(*p))
+  EXPECTED_DIGIT:
+    die("expected digit in '%s'", path);
+  char ch;
+  while ((ch = *p++) != ' ')
+    if (!isdigit(ch))
+      goto EXPECTED_DIGIT;
+    else
+      number = 10 * number + (ch - '0');
+  benchmark->number = number;
+  char *q = p;
+  while ((ch = *p) != ' ')
+    if (!ch)
+      die("unexpected end-of-line in '%s'", path);
+    else
+      p++;
+  *p++ = 0;
+  if (!(benchmark->path = strdup(q)))
+    out_of_memory("copying benchmark path in");
+  if (!(benchmark->name = strdup(p + 1)))
+    out_of_memory("copying benchmark name");
+}
+
+static void push_benchmark(struct benchmark *benchmark) {
+  if (size_benchmarks == capacity_benchmarks) {
+    capacity_benchmarks = capacity_benchmarks ? 2 * capacity_benchmarks : 1;
+    benchmarks = realloc(benchmarks, capacity_benchmarks * sizeof *benchmarks);
+    if (!benchmarks)
+      out_of_memory("reallocating benchmarks");
+  }
+  benchmarks[size_benchmarks++] = *benchmark;
+}
+
+static void parse_zummary(struct zummary *zummary, const char *path) {}
+
+static void push_zummary(struct zummary *zummary) {
+  if (size_zummaries == capacity_zummaries) {
+    capacity_zummaries = capacity_zummaries ? 2 * capacity_zummaries : 1;
+    zummaries = realloc(zummaries, capacity_zummaries * sizeof *zummaries);
+    if (!zummaries)
+      out_of_memory("reallocating zummaries");
+  }
+  zummaries[size_zummaries++] = *zummary;
+}
+
 int main(int argc, char **argv) {
   if (argc != 3) {
     fputs("usage: zort <benchmarks> <dir>\n", stderr);
@@ -91,6 +152,9 @@ int main(int argc, char **argv) {
   directory_path = argv[1];
   if (!file_exists(benchmarks_path))
     die("benchmarks file '%s' does not exist", benchmarks_path);
+  FILE *benchmarks_file = fopen(benchmarks_path, "r");
+  if (!benchmarks_file)
+    die("could not open and read '%s'", benchmarks_path);
   if (!directory_exists(directory_path))
     die("directory '%s' does not exist", directory_path);
   size_t zummary_path_len = strlen(directory_path) + strlen("/zummary") + 1;
@@ -103,11 +167,25 @@ int main(int argc, char **argv) {
   FILE *zummary_file = fopen(zummary_path, "r");
   if (!zummary_file)
     die("could not open and read '%s'", zummary_path);
+  while (!read_line(benchmarks_file, benchmarks_path)) {
+    struct benchmark benchmark;
+    parse_benchmark(&benchmark, benchmarks_path);
+    push_benchmark(&benchmark);
+  }
   if (!read_line(zummary_file, zummary_path))
     die("failed to read header line in '%s'", zummary_path);
   while (read_line(zummary_file, zummary_path)) {
+    struct zummary zummary;
+    parse_zummary(&zummary, zummary_path);
+    push_zummary(&zummary);
   }
+  fclose(benchmarks_file);
   fclose(zummary_file);
+  for (size_t i = 0; i != size_zummaries; i++)
+    free(benchmarks[i].name);
+  for (size_t i = 0; i != size_benchmarks; i++)
+    free(benchmarks[i].path), free(benchmarks[i].name);
+  free(benchmarks);
   free(zummary_path);
   free(line);
   return 0;
